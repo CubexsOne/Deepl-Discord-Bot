72b0afc08dc52db0883d464d78440c175e3854d7
Pascal Sauer <email@cubexs.dev>
2024-12-23 00:18:27 +0100

    feat(deepl-bot) Start discord bot

--- deepl-bot/src/main.ts
+++ deepl-bot/src/main.ts
@@ -1,9 +1,22 @@
 import * as dotenv from 'dotenv'
 import logger from './utils/logger'
+import { Discord } from './discord'
+import { environments } from './utils'
 dotenv.config()
 
 async function init(): Promise<void> {
-	logger.info('Deepl Translator Bot is starting...')
+	const bot = new Discord(
+		environments.DISCORD_TOKEN,
+		environments.DISCORD_CLIENT_ID,
+		environments.DISCORD_CLIENT_SECRET,
+	)
+
+	try {
+		logger.info('Deepl Translator Bot is starting...')
+		await bot.start()
+	} catch (error) {
+		logger.error(error)
+	}
 }
 
 init()
