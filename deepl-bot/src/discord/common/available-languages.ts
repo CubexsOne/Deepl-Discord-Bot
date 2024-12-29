6658a6d11956160bec253a08c20ae952a025f54b
Pascal Sauer <email@cubexs.dev>
2024-12-29 01:13:08 +0100

    chore: Add list of languages for translation

---
+++ deepl-bot/src/discord/common/available-languages.ts
@@ -0,0 +1,7 @@
+export const availableLanguages = [
+	{ name: 'Portuguese (Brazilian)', value: 'br' },
+	{ name: 'German', value: 'de' },
+	{ name: 'English (US)', value: 'en-US' },
+	{ name: 'English (UK)', value: 'en-GB' },
+	{ name: 'Spanish', value: 'es' },
+]
