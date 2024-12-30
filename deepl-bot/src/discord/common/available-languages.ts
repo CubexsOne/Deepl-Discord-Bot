41394265c7f6c29dbc3d6db1b6836843ffd5ab7b
Pascal Sauer <email@cubexs.dev>
2024-12-30 16:42:24 +0100

    feat: Add object to translate language value to flag

--- deepl-bot/src/discord/common/available-languages.ts
+++ deepl-bot/src/discord/common/available-languages.ts
@@ -1,7 +1,20 @@
-export const availableLanguages = [
+type AvailableLanguages = {
+	name: string
+	value: string
+}
+
+export const availableLanguages: AvailableLanguages[] = [
 	{ name: 'Portuguese (Brazilian)', value: 'br' },
 	{ name: 'German', value: 'de' },
 	{ name: 'English (US)', value: 'en-US' },
 	{ name: 'English (UK)', value: 'en-GB' },
 	{ name: 'Spanish', value: 'es' },
 ]
+
+export const languageFlags = {
+	br: '🇧🇷',
+	de: '🇩🇪',
+	'en-US': '🇺🇸',
+	'en-GB': '🇬🇧',
+	es: '🇪🇸',
+}
