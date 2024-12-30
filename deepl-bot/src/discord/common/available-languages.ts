72de62c9beb006287fa2fb86ed7bb23ca602da11
Pascal Sauer <email@cubexs.dev>
2024-12-30 19:07:29 +0100

    refactor: Use correct lang code for brazilian and replace the flags object with a mapping function

--- deepl-bot/src/discord/common/available-languages.ts
+++ deepl-bot/src/discord/common/available-languages.ts
@@ -4,17 +4,26 @@ type AvailableLanguages = {
 }
 
 export const availableLanguages: AvailableLanguages[] = [
-	{ name: 'Portuguese (Brazilian)', value: 'br' },
 	{ name: 'German', value: 'de' },
 	{ name: 'English (US)', value: 'en-US' },
 	{ name: 'English (UK)', value: 'en-GB' },
 	{ name: 'Spanish', value: 'es' },
+	{ name: 'Portuguese (Brazilian)', value: 'pt-BR' },
 ]
 
-export const languageFlags = {
-	br: '🇧🇷',
-	de: '🇩🇪',
-	'en-US': '🇺🇸',
-	'en-GB': '🇬🇧',
-	es: '🇪🇸',
+export function mapLanguageToFlag(language: string): string {
+	switch (language) {
+		case 'de':
+			return '🇩🇪'
+		case 'en-US':
+			return '🇺🇸'
+		case 'en-GB':
+			return '🇬🇧'
+		case 'es':
+			return '🇪🇸'
+		case 'pt-BR':
+			return '🇧🇷'
+		default:
+			return ''
+	}
 }
