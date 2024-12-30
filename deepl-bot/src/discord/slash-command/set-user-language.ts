63c63d97292a5b8d39f25ef2b73ba3a09fa54908
Pascal Sauer <email@cubexs.dev>
2024-12-30 17:53:15 +0100

    feat: Save user settings in database

--- deepl-bot/src/discord/slash-command/set-user-language.ts
+++ deepl-bot/src/discord/slash-command/set-user-language.ts
@@ -5,15 +5,18 @@ import {
 	SlashCommandBuilder,
 } from 'discord.js'
 import { availableLanguages } from '../common'
+import { PrismaClient } from '@prisma/client'
+import logger from '../../utils/logger'
 
 const COMMAND_NAME = 'language'
+const OPTION_NAME = 'target-language'
 export function setUserLanguageSlashCommand(): RESTPostAPIApplicationCommandsJSONBody {
 	const command = new SlashCommandBuilder()
 		.setName(COMMAND_NAME)
 		.setDescription('Set language for translation')
 		.addStringOption((option) =>
 			option
-				.setName('language')
+				.setName(OPTION_NAME)
 				.setDescription('Select language')
 				.setRequired(true)
 				.setChoices(availableLanguages),
@@ -29,10 +32,66 @@ export function setUserLanguageInteraction(client: Client): void {
 		if (interaction.commandName !== COMMAND_NAME) return
 		await interaction.deferReply({ ephemeral: true })
 
-		// TODO: Add storing value in database
+		const userId = interaction.user.id
+		const targetLanguage = interaction.options.getString(OPTION_NAME)!
+
+		const prisma = new PrismaClient()
+		try {
+			if (await hasUserSettings(prisma, userId)) {
+				await updateUserSettings(prisma, userId, targetLanguage)
+			} else {
+				await createUserSettings(prisma, userId, targetLanguage)
+			}
+		} catch (error) {
+			logger.error(error)
+			await interaction.editReply({
+				content: 'Error when setting the translation settings!',
+			})
+			return
+		} finally {
+			await prisma.$disconnect()
+		}
 
 		await interaction.editReply({
-			content: 'Language successful set!',
+			content: 'Translation settings successfully set!',
 		})
+	})
+}
+
+async function hasUserSettings(prisma: PrismaClient, userId: string): Promise<boolean> {
+	const userSettings = await prisma.userSettings.findUnique({
+		where: {
+			userId,
+		},
+	})
+
+	return !!userSettings
+}
+
+async function createUserSettings(
+	prisma: PrismaClient,
+	userId: string,
+	targetLanguage: string,
+): Promise<void> {
+	await prisma.userSettings.create({
+		data: {
+			userId,
+			targetLanguage,
+		},
+	})
+}
+
+async function updateUserSettings(
+	prisma: PrismaClient,
+	userId: string,
+	targetLanguage: string,
+): Promise<void> {
+	await prisma.userSettings.update({
+		where: {
+			userId,
+		},
+		data: {
+			targetLanguage,
+		},
 	})
 }
