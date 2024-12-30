98578a93a9b329e39f491ee6aac2686726d2818f
Pascal Sauer <email@cubexs.dev>
2024-12-30 16:42:04 +0100

    chore: Add emoji id as env var

--- deepl-bot/src/utils/environments.ts
+++ deepl-bot/src/utils/environments.ts
@@ -6,6 +6,7 @@ interface Environments {
 	DISCORD_CLIENT_ID: string
 	DISCORD_CLIENT_SECRET: string
 	DISCORD_TOKEN: string
+	DISCORD_TRANSLATE_EMOJI_ID: string
 	DEEPL_AUTH_KEY: string
 	NODE_ENV: string
 }
@@ -16,6 +17,7 @@ export const environments: Environments = {
 	DISCORD_CLIENT_ID: retrieveEnvironmentValue('DISCORD_CLIENT_ID'),
 	DISCORD_CLIENT_SECRET: retrieveEnvironmentValue('DISCORD_CLIENT_SECRET'),
 	DISCORD_TOKEN: retrieveEnvironmentValue('DISCORD_TOKEN'),
+	DISCORD_TRANSLATE_EMOJI_ID: retrieveEnvironmentValue('DISCORD_TRANSLATE_EMOJI_ID='),
 	DEEPL_AUTH_KEY: retrieveEnvironmentValue('DEEPL_AUTH_KEY'),
 	NODE_ENV: retrieveEnvironmentValue('NODE_ENV'),
 }
