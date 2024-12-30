001efaa99798771a147ee8189ed65a1cc60a29ca
Pascal Sauer <email@cubexs.dev>
2024-12-30 19:08:27 +0100

    feat: Add translation for messages

--- deepl-bot/src/discord/events/translate-message-on-reaction-event.ts
+++ deepl-bot/src/discord/events/translate-message-on-reaction-event.ts
@@ -2,6 +2,9 @@ import { Client, Colors, EmbedBuilder, Events } from 'discord.js'
 import { environments } from '../../utils'
 import logger from '../../utils/logger'
 import * as timers from 'node:timers/promises'
+import { getUserSettings, hasUserSettings } from '../../database/user-settings'
+import { mapLanguageToFlag } from '../common'
+import * as deepl from 'deepl-node'
 
 export function translateMessageOnReactionEvent(client: Client): void {
 	client.on(Events.MessageReactionAdd, async (interaction, user) => {
@@ -9,34 +12,50 @@ export function translateMessageOnReactionEvent(client: Client): void {
 		if (interaction.emoji.id !== environments.DISCORD_TRANSLATE_EMOJI_ID) return
 		if (!environments.DISCORD_CHANNELS_TO_LISTEN.includes(interaction.message.channelId)) return
 
+		if (interaction.partial) {
+			try {
+				await interaction.fetch()
+			} catch (error) {
+				logger.error(error)
+			}
+		}
+
 		const serverId = environments.DISCORD_SERVER_ID
-		const channeldId = interaction.message.channelId
+		const channelId = interaction.message.channelId
 		const messageId = interaction.message.id
-
-		const hasSettings = true
 		try {
-			// TODO: Retrieve language to to translate
-			// TODO: Translate via deepl and save in cache in database
-			// TODO: if user settings not existing, show hint to set settings ✅
-			if (!hasSettings) {
+			if (!(await hasUserSettings(user.id))) {
 				const message = await user.send({
-					embeds: [createNoSettingsEmbed(serverId, channeldId, messageId)],
+					embeds: [createNoSettingsEmbed(serverId, channelId, messageId)],
 				})
 				await timers.setTimeout(3_000)
 				await message.delete()
-			}
+			} else {
+				const { targetLanguage } = await getUserSettings(user.id)
+				const targetFlag = mapLanguageToFlag(targetLanguage)
+				const originalMessage = interaction.message.content
 
-			// TODO: if user settings existing, translate ✅
-			if (hasSettings) {
-				const message = await user.send({
-					embeds: [createTranslationEmbed(serverId, channeldId, messageId, '123', '123')],
+				const translator = new deepl.Translator(environments.DEEPL_AUTH_KEY)
+				const translationResult = await translator.translateText(
+					originalMessage || '',
+					null,
+					targetLanguage as deepl.TargetLanguageCode,
+				)
+				const originalFlag = mapLanguageToFlag(translationResult.detectedSourceLang)
+				await user.send({
+					embeds: [
+						createTranslationEmbed({
+							serverId,
+							channelId,
+							messageId,
+							originalFlag,
+							targetFlag,
+							originalMessage,
+							translatedMessage: translationResult.text,
+						}),
+					],
 				})
-				// TODO: REMOVE
-				await timers.setTimeout(3_000)
-				await message.delete()
 			}
-
-			// TODO: Remove reaction after sending translation or hint ✅
 		} catch (error) {
 			logger.error(error)
 		}
@@ -68,18 +87,30 @@ function createNoSettingsEmbed(
 	return embed
 }
 
-function createTranslationEmbed(
-	serverId: string,
-	channelId: string,
-	messageId: string,
-	originalMessage: string,
-	translatedMessage: string,
-): EmbedBuilder {
+function createTranslationEmbed(props: {
+	serverId: string
+	channelId: string
+	messageId: string
+	originalFlag: string
+	targetFlag: string
+	originalMessage: string | null
+	translatedMessage: string | null
+}): EmbedBuilder {
+	const {
+		serverId,
+		channelId,
+		messageId,
+		originalFlag,
+		targetFlag,
+		originalMessage,
+		translatedMessage,
+	} = props
+
 	const embed = new EmbedBuilder()
-		.setTitle('Translation')
+		.setTitle(`Translation (${originalFlag} -> ${targetFlag})`)
 		.setColor(Colors.Navy)
-		.addFields({ name: 'Original Message:', value: originalMessage })
-		.addFields({ name: 'Translated Message:', value: translatedMessage })
+		.addFields({ name: 'Original Message:', value: originalMessage || '\u200B' })
+		.addFields({ name: 'Translated Message:', value: translatedMessage || '\u200B' })
 		.addFields({
 			name: 'Related message:',
 			value: `https://discordapp.com/channels/${serverId}/${channelId}/${messageId}`,
